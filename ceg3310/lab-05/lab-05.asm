c5d0ee4de44e3b1735ad3b378b6ddf95a06ed8dc
Hannah Wysong <hannahrock03@gmail.com>
2025-01-13 15:05:20 -0500

    getting caught up

--- ceg3310/lab-05/lab-05.asm
+++ ceg3310/lab-05/lab-05.asm
@@ -6,156 +6,201 @@ LD R6, STACK_PTR            				; LOAD the pointer to the bottom of the stack in
 LEA R4, GLOBAL_VARS            				; MAKE your global var pointer R4 point to globals    (R4 = ADDRESS(GLOBAL_VARS))
 
 ADD R6, R6, #-1
-STR R7, R6, #0						; push return address #5FFF
+STR R7, R6, #0						
 
 ADD R6, R6, #-1
-STR R5, R6, #0						; push prev frame pointer #5FFE
-ADD R5, R6, #0						; set frame pointer 
+STR R5, R6, #0						
+ADD R5, R6, #0						
 
-ADD R6, R6, #-1 					; #5FFD
+ADD R6, R6, #-1 					
 AND R0, R0, #0 
-STR R0, R6, #0						; push int n=0
-ADD R6, R6, #-1						; #5FFC
-STR R0, R6, #0 						; push int result=0
+STR R0, R6, #0						
+ADD R6, R6, #-1						
+STR R0, R6, #0 						
 
 ADD R0, R4, #0 
-ADD R0, R0, #4						; loads prompt1 into R0
-PUTS							; print prompt1 
+ADD R0, R0, #4						
+PUTS							
 
 AND R0, R0, #0
-ADD R0, R4, #2 						; load neg ascii into R0
+ADD R0, R4, #2 						
 LDR R0, R0, #0 
 
-ADD R6, R6, #-1 					; #5FFB push getnum return value
+ADD R6, R6, #-1 					
 
 JSR GETNUM 
 
 LDR R0, R6, #0
-STR R0, R5, #-1 					; store getnum() into n
+STR R0, R5, #-1 					
 
-ADD R6, R6, #1 					    ; pop getnum return value
+ADD R6, R6, #1 					    
 
 ADD R6, R6, #-1
 STR R0, R5, #-1 
-STR R0, R6, #0 						; push fibonacci arg
+STR R0, R6, #0 						
 
-ADD R6, R6, #-1                     ; push fibonacci return value
 
-JSR FIBONACCI 
+JSR FIBONACCI
 
 LDR R0, R6, #0
-STR R0, R5, #2 						; store fibonacci into result
+STR R0, R5, #2 						
 
-ADD R6, R6, #1 						; pop result
-ADD R6, R6, #1 						; pop return value
+ADD R6, R6, #1 						
+ADD R6, R6, #1 						
 
-ADD R0, R4, #15
+ADD R0, R4, #0
 ADD R0, R0, #15
-ADD R0, R0, #13
-;PUTS							; print first part of response
+ADD R0, R0, #15
+PUTS  
 
+ADD R6, R6, #-1
+LDR R0, R5, #-1
+STR R0, R6, #0 
 
+JSR DISPLAY
+
+ADD R6, R6, #1 
+
+ADD R0, R4, #0
+ADD R0, R0, #15
+ADD R0, R0, #15
+ADD R0, R0, #4
+PUTS  
+
+ADD R6, R6, #-1
+LDR R0, R5, #2
+STR R0, R6, #0 
+
+JSR DISPLAY
+
+ADD R6, R6, #1 
+ADD R6, R6, #1 
+ADD R6, R6, #1 
+LDR R5, R6, #0
+ADD R6, R6, #1 
+LDR R7, R6, #0
+ADD R6, R6, #1 
 HALT
 
 GLOBAL_VARS                    		; Your global variables start here 
 ASCII_OFFSET    .FILL x0030         ; MAX_ARRAY_SIZE is a global variable and predefined
 STACK_PTR    .FILL x6000            ; STACK_PTR is a pointer to the bottom of the stack (x6000)
-NEG_ASCII .FILL #-48 				; 0
-ASCII .FILL x30 					; 1
-PROMPT1 .STRINGZ "Please enter a number n: "	; 2
-RESULT1 .STRINGZ "\nF(" 			; 28
-RESULT2 .STRINGZ ") = "				; 32
+NEG_ASCII .FILL #-48 				; 2
+ASCII .FILL x30 					; 3
+PROMPT1 .STRINGZ "Please enter a number n: "	; 4
+RESULT1 .STRINGZ "\nF(" 			; 30
+RESULT2 .STRINGZ ") = "				; 34
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-FIBONACCI
+FIBONACCI 
 ;    Your fibonacci() function starts here
-;    f(n) = F(n-1) + F(n-2)
-;    where F(0) = 0 and F(1) = 1
-ADD R6, R6, #-1
-STR R7, R6, #0 ; push return addr 
-ADD R6, R6, #-1
-STR R5, R6, #0 ; push prev frame ptr
-ADD R5, R6, #0
 
-ADD R6, R6, #-1 ; push fibonacci input
+;;;;
+; Return value
+ADD R6, R6, #-1             
 
-; BASE CASES
-; checking n == 0
+ADD R6, R6, #-1             
+STR R7, R6, #0              
 
-ADD R0, R0, #1
-ADD R0, R0, #-1
-BRz END_CASE
-; checking n == 1
-ADD R0, R0, #-1 
-BRz END_CASE 
-ADD R0, R0, #1
+ADD R6, R6, #-1             
+STR R5, R6, #0              
+ADD R5, R6, #0              
 
+ADD R6, R6, #-1             
+STR R0, R6, #0              
+ADD R6, R6, #-1             
+STR R1, R6, #0              
+ADD R6, R6, #-1             
+STR R2, R6, #0              
+ADD R6, R6, #-1             
+STR R3, R6, #0              
+ADD R6, R6, #-1             
+
+;;;;
+; BASE CASE
+LDR R0, R5, #3              
+ADD R1, R0, #-1             
+BRz END_CASE                
+ADD R0, R0, #1              
+ADD R0, R0, #-1             
+BRz END_CASE                
+
+;;;;
 ; GENERAL CASE
-ADD R0, R0, #-1
-STR R0, R6, #0
+ADD R0, R0, #-1             
+STR R0, R6, #0              
 JSR FIBONACCI
-LDR R1, R5, #-3
+LDR R1, R5, #-6             
 
-ADD R0, R0, #-2
-STR R0, R6, #0
+ADD R0, R0, #-1             
+STR R0, R6, #0              
 JSR FIBONACCI
-LDR R2, R5, #-3
+LDR R2, R5, #-6             
 
-ADD R0, R1, R2
+ADD R0, R1, R2              
+
+;;;;
 ; END CASE
 END_CASE
-STR R0, R5, #2  ; store n in return value
+STR R0, R5, #2              
 
-ADD R6, R6, #1  ; pop input 
+ADD R6, R6, #1             
+LDR R3, R6, #0              
+ADD R6, R6, #1              
+LDR R2, R6, #0              
+ADD R6, R6, #1              
+LDR R1, R6, #0              
+ADD R6, R6, #1              
+LDR R0, R6, #0              
 
-LDR R5, R6, #0			; restore prev frame pointer 
-ADD R6, R6, #1				
+ADD R6, R6, #1              
+LDR R5, R6, #0              
 
-LDR R7, R6, #0			; restore prev return address 
-ADD R6, R6, #1 	
+ADD R6, R6, #1             
+LDR R7, R6, #0              
+
+ADD R6, R6, #2              
 RET
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 GETNUM
 ;    Your getnum() function starts here
-ADD R6, R6, #-1			; push previous return address x5FFA
+ADD R6, R6, #-1			
 STR R7, R6, #0
 
-ADD R6, R6, #-1			;push previous frame pointer x5FF9
+ADD R6, R6, #-1			
 STR R5, R6, #0
-ADD R5, R6, #0			; set frame pointer
+ADD R5, R6, #0			
 
 ADD R6, R6, #-1 
-STR R0, R6, #0			; save R0 x5FF8
+STR R0, R6, #0			
 
 ADD R6, R6, #-1
-STR R1, R6, #0 			; save R1 x5FF7
+STR R1, R6, #0 			
 
 ;; logic
-AND R0, R0, #0 			; set R0 to zero
+AND R0, R0, #0 			
 GETC
 OUT 
 
-LDR R1, R5, #-1			; load ascii offset into R1
-ADD R0, R0, R1 			; convert to ascii to int
+LDR R1, R5, #-1			
+ADD R0, R0, R1 			
 
 ;; logic end 
 
-STR R0, R5, #2			; store R0 in int input
+STR R0, R5, #2			
 
 LDR R1, R6, #0
-ADD R6, R6, #1 			; pop R1
+ADD R6, R6, #1 			
 
 LDR R0, R6, #0
-ADD R6, R6, #1 			; pop R0
+ADD R6, R6, #1 			
 
-LDR R5, R6, #0			; restore prev frame pointer 
+LDR R5, R6, #0			
 ADD R6, R6, #1				
 
-LDR R7, R6, #0			; restore prev return address 
+LDR R7, R6, #0			 
 ADD R6, R6, #1 	
 RET
 
@@ -165,7 +210,43 @@ RET
 
 DISPLAY
 ;    Your display() function starts here
-RET
+ADD R6, R6, #-1
+STR R7, R6, #0 
+ADD R6, R6, #-1
+STR R5, R6, #0 
+ADD R5, R6, #0
 
+AND R0, R0, #0
+ADD R6, R6, #-1 
+STR R0, R6, #0
+
+LDR R3, R4, #3 
+AND R2, R2, #0 
+ADD R2, R2, #-1 
+ADD R0, R0, #-10 
+LDR R1, R5, #2 
+
+TEN_LOOP
+    ADD R2, R2, #1 
+    ADD R1, R1, R0 
+BRzp TEN_LOOP
+ADD R2, R2, #0 
+BRz SKIP_TEN
+LDR R0, R4, #1 
+ADD R0, R2, R3 
+OUT
+
+SKIP_TEN
+    ADD R1, R1, #10
+    ADD R0, R1, R3
+    OUT
+
+ADD R6, R6, #1 
+
+LDR R5, R6, #0
+ADD R6, R6, #1 
+LDR R7, R6, #0
+ADD R6, R6, #1 
+RET
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .END
