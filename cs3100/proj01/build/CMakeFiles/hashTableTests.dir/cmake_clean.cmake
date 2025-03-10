a684ea3bd47fe64a6f66b8fb646bad637f0aebf0
Hannah Wysong <hannahrock03@gmail.com>
2025-03-10 11:57:53 -0400

    build file

---
+++ cs3100/proj01/build/CMakeFiles/hashTableTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hashTableTests.dir/HashTable.cpp.o"
+  "CMakeFiles/hashTableTests.dir/HashTable.cpp.o.d"
+  "CMakeFiles/hashTableTests.dir/HashTableTests.cpp.o"
+  "CMakeFiles/hashTableTests.dir/HashTableTests.cpp.o.d"
+  "hashTableTests"
+  "hashTableTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hashTableTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
