4209090185be716940a9ed4f4a1476b73d1f6bf3
Hannah Wysong <hannahrock03@gmail.com>
2025-03-10 11:16:20 -0400

    reset

--- cs3100/proj01/HashTableTests.cpp
+++ cs3100/proj01/HashTableTests.cpp
@@ -20,7 +20,6 @@ const string evalName{""};
 #else
 #define OUTSTREAM cout
 #endif
-#include <optional>
 
 #define HT_PRINT(ht) OUTSTREAM << "HashTable contents" << endl; OUTSTREAM << "------------------" << endl; OUTSTREAM << ht1 << endl;
 
