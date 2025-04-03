3b342ad2eebd0742d49c0b3b8e4cf704758acce4
Hannah Wysong <hannahrock03@gmail.com>
2025-04-03 11:36:14 -0400

    new edits

---
+++ cs3100/proj02/cmake-build-debug/CMakeFiles/Project2_AVLTree.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2_AVLTree.dir/AVLTree.cpp.obj"
+  "CMakeFiles/Project2_AVLTree.dir/AVLTree.cpp.obj.d"
+  "CMakeFiles/Project2_AVLTree.dir/AVLTreeDriver.cpp.obj"
+  "CMakeFiles/Project2_AVLTree.dir/AVLTreeDriver.cpp.obj.d"
+  "Project2_AVLTree.exe"
+  "Project2_AVLTree.exe.manifest"
+  "Project2_AVLTree.pdb"
+  "libProject2_AVLTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2_AVLTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cs3100/proj02/cmake-build-debug/CMakeFiles/Project2_AVLTree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Project2_AVLTree.
+# This may be replaced when dependencies are built.
